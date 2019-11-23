1e89318ed4994d215ef2a39970fa44690c698d28
Priyashan Jayasankha <priyashanshell@gmail.com>
2019-11-23 17:44:07 +0530

    some imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,19 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { ViewEventComponent } from './components/view-event/view-event.component';
+import { LoginComponent } from './components/login/login.component';
+import { StudentRegisterComponent } from './components/student-register/student-register.component';
 
 // Services
 import { EventDetailsService } from './services/event-details.service';
+import { LoginRegisterService } from './services/login-register.service';
 
 // Others
 import { AppRoutingModule , routingComponents } from './app-routing.module';
 import { environment } from 'src/environments/environment';
-import { HomeComponent } from './components/home/home.component';
-import { CardComponent } from './components/card/card.component';
-import { ViewEventComponent } from './components/view-event/view-event.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +34,8 @@ import { ViewEventComponent } from './components/view-event/view-event.component
     SidebarComponent,
     HomeComponent,
     CardComponent,
+    LoginComponent,
+    StudentRegisterComponent,
   ],
   imports: [
     BrowserModule,
@@ -38,7 +43,7 @@ import { ViewEventComponent } from './components/view-event/view-event.component
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
   ],
-  providers: [EventDetailsService],
+  providers: [EventDetailsService, LoginRegisterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
