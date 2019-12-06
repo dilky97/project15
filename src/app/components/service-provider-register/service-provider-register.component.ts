af595a66f17f83747d8c11b79bbaa23153e1c5d6
Priyashan Jayasankha <priyashanshell@gmail.com>
2019-12-06 09:06:37 +0530

    service provider register page

---
+++ src/app/components/service-provider-register/service-provider-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-provider-register',
+  templateUrl: './service-provider-register.component.html',
+  styleUrls: ['./service-provider-register.component.scss']
+})
+export class ServiceProviderRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
