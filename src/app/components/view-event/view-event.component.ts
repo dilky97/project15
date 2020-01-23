bceaa3d72819cc0d8740c72d9dcab60dd10d6541
Priyashan Jayasankha <priyashanshell@gmail.com>
2020-01-23 08:10:27 +0530

    added subscribe button for students

--- src/app/components/view-event/view-event.component.ts
+++ src/app/components/view-event/view-event.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventDetailsService } from 'src/app/services/event-details.service';
+import * as firebase from 'firebase/app';
+import { StudentDetails } from 'src/app/models/student-details.model';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { EventDetails } from 'src/app/models/event-details.model';
 
 @Component({
   selector: 'app-view-event',
@@ -10,14 +14,48 @@ import { EventDetailsService } from 'src/app/services/event-details.service';
 export class ViewEventComponent implements OnInit {
 
   selectedEventId: string;
-  eventDetailsObject: any;
+  event = {} as EventDetails;
+  student = {} as StudentDetails;
 
-  constructor( private route: ActivatedRoute , private eventDetails: EventDetailsService ) { }
+  isGoing: number ;
+
+  constructor( private route: ActivatedRoute, private eventDetails: EventDetailsService, private firestore: AngularFirestore ) { }
 
   ngOnInit() {
     this.selectedEventId = this.route.snapshot.paramMap.get('id');
     this.eventDetails.getViewEvent(this.selectedEventId).subscribe( temp => {
-      this.eventDetailsObject = temp;
+      this.event = temp as EventDetails ;
+    });
+
+    firebase.auth().onAuthStateChanged( user => {
+      const promise = this.firestore.firestore.collection('students').doc(user.uid).get();
+      promise.then( snapshot => {
+        this.student =  snapshot.data() as StudentDetails;
+        if (this.student.participatingEvents.includes(this.selectedEventId)) {
+          this.isGoing = 1 ;
+        } else {
+          this.isGoing = 0 ;
+        }
+      });
+    });
+
+  }
+
+  changeGoing() {
+    firebase.auth().onAuthStateChanged( user => {
+      const promise = this.firestore.firestore.collection('students').doc(user.uid).get();
+      promise.then( snapshot => {
+        this.student =  snapshot.data() as StudentDetails;
+        if (this.isGoing === 0) {
+          this.student.participatingEvents.push(this.selectedEventId);
+          this.firestore.collection('students').doc(user.uid).update(this.student);
+          this.isGoing = 1 ;
+        } else if (this.isGoing === 1) {
+          this.student.participatingEvents.splice( this.student.participatingEvents.indexOf(this.selectedEventId), 1 );
+          this.firestore.collection('students').doc(user.uid).update(this.student);
+          this.isGoing = 0 ;
+        }
+      });
     });
   }
 
